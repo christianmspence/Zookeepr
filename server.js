d68a649002724008278cf492be223b0bca0dd4b0
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-07-05 17:42:09 -0400

    Issue 2 completed

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -51,9 +53,34 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    } if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    } if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    } if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    //our funtion's main code will go here!
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -65,9 +92,17 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the nex index of the array will be
+    req.body.id = animals.length.toString();
+
+    //if any daya in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.')
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(req.body);
+    }
 });
 
 app.get('/api/animals/:id', (req, res) => {
